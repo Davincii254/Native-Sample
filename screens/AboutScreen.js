ff31ec3972843bc6b06b1e361f069d7d7b4ba6e7
Davincii254 <victorkuria7@gmail.com>
2024-01-31 18:09:35 +0300

    Update both contact and about screen with text

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,5 +1,8 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import { ScrollView } from "react-native-gesture-handler";
+import PARTNERS from "../shared/partners";
+import { useState } from "react";
+import { Avatar, ListItem } from "react-native-elements";
 
 
 const AboutNavigator = ()  => {
@@ -15,9 +18,31 @@ const AboutNavigator = ()  => {
 }
 
 const AboutScreen = () => {
+    const [partners, setPartners] = useState(PARTNERS);
+
+
     return (
         <ScrollView>
-
+            <Card>
+                <Card.Title>Our Mission</Card.Title>
+                <Card.Divider />
+                <Text style={{ margin: 10 }}>
+                    We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
+                </Text>
+            </Card>
+            <Card>
+                <Card.Title>Community Partners</Card.Title>
+                <Card.Divider />
+                {partners.map(partner => (
+                    <ListItem key={partner.id}>
+                        <Avatar rounded source={{uri: partner.image}}/>
+                        <ListItem.Content>
+                            <ListItem.Title>{partner.name}</ListItem.Title>
+                            <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
+                        </ListItem.Content>
+                    </ListItem>
+                ))}
+            </Card>
         </ScrollView>
     )
 }
--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -15,7 +15,17 @@ const ContactNavigator = ()  => {
 const ContactScreen = () => {
     return (
         <ScrollView>
-
+            <Card wrapperStyle={{margin: 20}}>
+                <Card.Title>
+                    Contact Information
+                </Card.Title>
+                <Card.Divider/>
+                <Text>1 Nucamp Way</Text>
+                <Text>Seattle, WA 98001</Text>
+                <Text>U.S.A.</Text>
+                <Text style={{ marginBottom: 10 }}>Phone: 1-206-555-1234</Text>
+                <Text>Email: campsites@nucamp.co</Text>
+            </Card>
         </ScrollView>
     )
 }
